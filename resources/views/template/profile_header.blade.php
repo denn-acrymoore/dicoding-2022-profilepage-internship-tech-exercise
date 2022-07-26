b5aa3bff7c68e0ece81250f81efc40628281b2e1
denn-acrymoore <denn.sebastian.e@gmail.com>
2022-07-26 15:50:59 +0700

    Added template page with header and content (incomplete)
    Added the 'profile_header.blade.php' that contains
    the main page (header, card, content background, footer).
    It is currently incomplete (only showing the dicoding logo,
    the 'academy' dropdown, and a simple dark blue background).

---
+++ resources/views/template/profile_header.blade.php
@@ -0,0 +1,15 @@
+<nav class="container-fluid dicoding-navbar">
+    <div class="col-3 dicoding-navbar-col-align-right">
+        <img class="dicoding-navbar-logo" src={{ asset('images/dicoding-header-logo.png') }} />
+    </div>
+    <div class="col-3 dicoding-navbar-col-align-left">
+        <div class="academy-dropdown">
+            <div class="academy-dropbtn">Academy</div>
+            <div class="academy-dropdown-content">
+                <a href="/">Progress Belajar</a>
+                <a href="/">Langganan</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-6 dicoding-navbar-col-align-right"></div>
+</nav>
