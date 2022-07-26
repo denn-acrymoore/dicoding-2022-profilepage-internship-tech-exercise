030dc175abe974ee88d8ac6d3a3f57376d7fe5f8
denn-acrymoore <denn.sebastian.e@gmail.com>
2022-07-26 17:28:59 +0700

    Added caret for "Academy" link in navbar and animation
    Changes:
    - Added the caret icon next to "Academy" link in navbar.
    - Added caret spin transition when hovered.
    - Added "Academy" dropdown opacity transition when hovered.

--- resources/views/template/profile_header.blade.php
+++ resources/views/template/profile_header.blade.php
@@ -4,7 +4,10 @@
     </div>
     <div class="col-3 dicoding-navbar-col-align-left">
         <div class="academy-dropdown">
-            <div class="academy-dropbtn">Academy</div>
+            <div class="academy-dropbtn">
+                Academy
+                <img class="caret-dropdown" src={{ asset('images/caret_icon.svg') }} width="10px"/>
+            </div>
             <div class="academy-dropdown-content">
                 <a href="/">Progress Belajar</a>
                 <a href="/">Langganan</a>
