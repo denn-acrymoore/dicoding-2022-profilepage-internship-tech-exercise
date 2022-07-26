d49a3857df8033ebad3aea8ccb6d8b43ea9a9ab8
denn-acrymoore <denn.sebastian.e@gmail.com>
2022-07-26 19:12:30 +0700

    Finish making the profile profile_header.blade.php
    Changes:
    - Separated the css file of profile_page for main,
      header, content, and footer.
    - Added fully animated avatar dropdown, notification button, and dashboard
      button.

--- resources/views/template/profile_header.blade.php
+++ resources/views/template/profile_header.blade.php
@@ -1,12 +1,10 @@
-<nav class="container-fluid dicoding-navbar">
-    <div class="col-3 dicoding-navbar-col-align-right">
-        <img class="dicoding-navbar-logo" src={{ asset('images/dicoding-header-logo.png') }} />
-    </div>
-    <div class="col-3 dicoding-navbar-col-align-left">
+<nav class="container-lg dicoding-navbar">
+    <div class="col-5 dicoding-navbar-col-align-left">
+        <img class="dicoding-navbar-logo" src="{{ asset('images/dicoding-header-logo.png') }}" />
         <div class="academy-dropdown">
             <div class="academy-dropbtn">
                 Academy
-                <img class="caret-dropdown" src={{ asset('images/caret_icon.svg') }} width="10px"/>
+                <img class="caret-dropdown" src="{{ asset('images/caret_icon.svg') }}" />
             </div>
             <div class="academy-dropdown-content">
                 <a href="/">Progress Belajar</a>
@@ -14,5 +12,26 @@
             </div>
         </div>
     </div>
-    <div class="col-6 dicoding-navbar-col-align-right"></div>
+    <div class="col-7 dicoding-navbar-col-align-right">
+        <div class='avatar-dropdown'>
+            <div class="avatar-dropbtn">
+                <img class="avatar-img" src="{{ asset('database-images/gigachad.jpg') }}" >
+                <img class="caret-dropdown" src="{{ asset('images/caret_icon.svg') }}" />
+            </div>
+            <div class="avatar-dropdown-content">
+                <a href="/">Daftar Pesanan</a>
+                <a href="/">Pengaturan</a>
+                <a href="/">Keluar</a>
+            </div>
+        </div>
+        <a class="bell-link" href="/">
+            <img class="bell-logo" src={{ asset('images/bell_icon.svg') }}>
+        </a>
+        <a class="dashboard-link" href="/">
+            <button class="dashboard-btn">
+                Dashboard
+                <img class="dashboard-btn-icon" src="{{ asset('images/home_icon.svg') }}">
+            </button>
+        </a>
+    </div>
 </nav>
--- resources/views/template/profile_page_template.blade.php
+++ resources/views/template/profile_page_template.blade.php
@@ -12,7 +12,10 @@
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="{{ asset('css/profile_page_template.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/profile_page_template/profile_page_main.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/profile_page_template/profile_page_header.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/profile_page_template/profile_page_content.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/profile_page_template/profile_page_footer.css') }}">
 </head>
 <body>
     {{-- Note: asset() -> website domain + filename --}}
