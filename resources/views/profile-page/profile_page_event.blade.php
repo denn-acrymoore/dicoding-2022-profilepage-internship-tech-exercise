f490562f4cb099074bff1fceb0465471f8e547a1
denn-acrymoore <denn.sebastian.e@gmail.com>
2022-07-28 09:50:26 +0700

    Added Event Section in Profile Page
    Changes:
    - Added finished 'event section' in profile page.
    - Make the icons in 'academy section' more responsive (will wrap
      when the card gets smaller).

--- resources/views/profile-page/profile_page_event.blade.php
+++ resources/views/profile-page/profile_page_event.blade.php
@@ -3,7 +3,26 @@
 @section('title', 'Profile Page - Event')
 
 @section('content')
-event
+<div class="event-section col-12">
+
+  <div class="card event-card">
+    <div class="card-body">
+      <span class="event-card-title">Tips Sukses Berkarir Sebagai Developer dan Programmer 2021</span>
+    </div>
+    <div class="card-footer bg-transparent event-card-footer">
+      <div class="event-badge">Seminar</div>
+    </div>
+  </div>
+  <div class="card event-card">
+    <div class="card-body">
+      <span class="event-card-title">Teknologi Baru Android di Google I/O 2021</span>
+    </div>
+    <div class="card-footer bg-transparent event-card-footer">
+      <div class="event-badge">Seminar</div>
+    </div>
+  </div>
+
+</div>
 
 
 @endsection
--- resources/views/template/profile_header.blade.php
+++ resources/views/template/profile_header.blade.php
@@ -1,38 +1,38 @@
 <nav class="container-lg dicoding-navbar">
-    <div class="col-5 dicoding-navbar-col-align-left">
-        <img class="dicoding-navbar-logo" src="{{ asset('images/dicoding-header-logo.png') }}" />
-        <div class="academy-dropdown">
-            <div class="academy-dropbtn">
-                Academy
-                <img class="caret-dropdown" src="{{ asset('images/header/caret_icon.svg') }}" />
-            </div>
-            <div class="academy-dropdown-content">
-                <a href="/">Progress Belajar</a>
-                <a href="/">Langganan</a>
-            </div>
-        </div>
+  <div class="col-5 dicoding-navbar-col-align-left">
+    <img class="dicoding-navbar-logo" src="{{ asset('images/dicoding-header-logo.png') }}" />
+    <div class="academy-dropdown">
+      <div class="academy-dropbtn">
+        Academy
+        <img class="caret-dropdown" src="{{ asset('images/header/caret_icon.svg') }}" />
+      </div>
+      <div class="academy-dropdown-content">
+        <a href="/">Progress Belajar</a>
+        <a href="/">Langganan</a>
+      </div>
     </div>
-    <div class="col-7 dicoding-navbar-col-align-right">
-        <div class='avatar-dropdown'>
-            <div class="avatar-dropbtn">
-                <img class="avatar-img" src="{{ asset('database-images/gigachad.jpg') }}" >
-                <img class="caret-dropdown" src="{{ asset('images/header/caret_icon.svg') }}" />
-            </div>
-            <div class="avatar-dropdown-content">
-                <a href="/">Daftar Pesanan</a>
-                <a href="/">Pengaturan</a>
-                <a href="/">Keluar</a>
-            </div>
-        </div>
-        <a class="bell-link" href="/">
-            <img class="bell-logo" src={{ asset('images/header/bell_icon.svg') }}>
-        </a>
-        <div class="vertical-line"></div>
-        <a class="dashboard-link" href="/">
-            <div class="dashboard-btn">
-                Dashboard
-                <img class="dashboard-btn-icon" src="{{ asset('images/header/home_icon.svg') }}">
-            </div>
-        </a>
+  </div>
+  <div class="col-7 dicoding-navbar-col-align-right">
+    <div class='avatar-dropdown'>
+      <div class="avatar-dropbtn">
+        <img class="avatar-img" src="{{ asset('database-images/gigachad.jpg') }}" >
+        <img class="caret-dropdown" src="{{ asset('images/header/caret_icon.svg') }}" />
+      </div>
+      <div class="avatar-dropdown-content">
+        <a href="/">Daftar Pesanan</a>
+        <a href="/">Pengaturan</a>
+        <a href="/">Keluar</a>
+      </div>
     </div>
+    <a class="bell-link" href="/">
+      <img class="bell-logo" src={{ asset('images/header/bell_icon.svg') }}>
+    </a>
+    <div class="vertical-line"></div>
+    <a class="dashboard-link" href="/">
+      <div class="dashboard-btn">
+        Dashboard
+        <img class="dashboard-btn-icon" src="{{ asset('images/header/home_icon.svg') }}">
+      </div>
+    </a>
+  </div>
 </nav>
