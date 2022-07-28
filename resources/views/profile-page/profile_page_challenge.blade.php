a317e1f8b3c16977ec47197555fed57eab83974d
denn-acrymoore <denn.sebastian.e@gmail.com>
2022-07-28 10:31:51 +0700

    Added finished Challenge Section in Profile Page
    Changes:
    - Added finished 'challenge section' in profile page.

--- resources/views/profile-page/profile_page_challenge.blade.php
+++ resources/views/profile-page/profile_page_challenge.blade.php
@@ -4,6 +4,22 @@
 
 @section('content')
 <div class="challenge-section col-12">
-
+  <div class="card challenge-card">
+    <div class="card-body">
+      <img class="challenge-card-image" src="{{ asset('images/challenge-page/makan-tanpa-sisa-bankdbs.jpg') }}">
+      <span class="challenge-title">#MakanTanpaSisa Instagram AR Filter Competition</span>
+      <span class="challenge-organizer">Diselenggarakan oleh: Bank DBS Indonesia</span>
+      <div class="challenge-status-div">
+        <img class="challenge-status-icon" src="{{ asset('images/challenge-page/trophy-icon-color.svg') }}" alt="">
+        <span class="challenge-status-text">Winner</span>
+      </div>
+    </div>
+    <div class="card-footer bg-transparent challenge-card-footer">
+      <div class="challenge-link-div">
+        <span class="challenge-link-text">Detail</span>
+        <img class="challenge-link-arrow" src="{{ asset('images/challenge-page/link-arrow-icon.svg') }}">
+      </div>
+    </div>
+  </div>
 </div>
 @endsection
